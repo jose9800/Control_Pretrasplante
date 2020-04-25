f3cbabdca21f1fa08d98f8bbc0f3eb93af4d8a17
Abr06 <61751911+Abr06@users.noreply.github.com>
2020-04-25 16:14:38 -0700

    submenu pacientes
    Creacin de formulario para lista pacientes y creacion de submenu de paceintes

--- Control_PreTrasplante/Capa_Presentacion/Formularios/Inicio.Designer.cs
+++ Control_PreTrasplante/Capa_Presentacion/Formularios/Inicio.Designer.cs
@@ -37,14 +37,20 @@
             this.panel_logo = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel_central = new System.Windows.Forms.Panel();
+            this.submenu_paciente = new System.Windows.Forms.Panel();
+            this.lista_paciente = new System.Windows.Forms.Button();
+            this.nuevo_paciente = new System.Windows.Forms.Button();
             this.panel_MenuLateral.SuspendLayout();
             this.panel_logo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.submenu_paciente.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel_MenuLateral
             // 
+            this.panel_MenuLateral.AutoScroll = true;
             this.panel_MenuLateral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(137)))), ((int)(((byte)(155)))));
+            this.panel_MenuLateral.Controls.Add(this.submenu_paciente);
             this.panel_MenuLateral.Controls.Add(this.iconbtn_paciente);
             this.panel_MenuLateral.Controls.Add(this.iconbtn_estudios);
             this.panel_MenuLateral.Controls.Add(this.iconbtn_reportes);
@@ -52,8 +58,9 @@
             this.panel_MenuLateral.Controls.Add(this.panel_logo);
             this.panel_MenuLateral.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel_MenuLateral.Location = new System.Drawing.Point(0, 0);
+            this.panel_MenuLateral.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel_MenuLateral.Name = "panel_MenuLateral";
-            this.panel_MenuLateral.Size = new System.Drawing.Size(255, 761);
+            this.panel_MenuLateral.Size = new System.Drawing.Size(340, 937);
             this.panel_MenuLateral.TabIndex = 0;
             // 
             // iconbtn_paciente
@@ -66,10 +73,11 @@
             this.iconbtn_paciente.IconChar = FontAwesome.Sharp.IconChar.UserInjured;
             this.iconbtn_paciente.IconColor = System.Drawing.Color.White;
             this.iconbtn_paciente.IconSize = 30;
-            this.iconbtn_paciente.Location = new System.Drawing.Point(0, 277);
+            this.iconbtn_paciente.Location = new System.Drawing.Point(0, 342);
+            this.iconbtn_paciente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.iconbtn_paciente.Name = "iconbtn_paciente";
             this.iconbtn_paciente.Rotation = 0D;
-            this.iconbtn_paciente.Size = new System.Drawing.Size(255, 50);
+            this.iconbtn_paciente.Size = new System.Drawing.Size(340, 62);
             this.iconbtn_paciente.TabIndex = 3;
             this.iconbtn_paciente.Text = "Paciente";
             this.iconbtn_paciente.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -86,10 +94,11 @@
             this.iconbtn_estudios.IconChar = FontAwesome.Sharp.IconChar.BookMedical;
             this.iconbtn_estudios.IconColor = System.Drawing.Color.White;
             this.iconbtn_estudios.IconSize = 30;
-            this.iconbtn_estudios.Location = new System.Drawing.Point(0, 227);
+            this.iconbtn_estudios.Location = new System.Drawing.Point(0, 280);
+            this.iconbtn_estudios.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.iconbtn_estudios.Name = "iconbtn_estudios";
             this.iconbtn_estudios.Rotation = 0D;
-            this.iconbtn_estudios.Size = new System.Drawing.Size(255, 50);
+            this.iconbtn_estudios.Size = new System.Drawing.Size(340, 62);
             this.iconbtn_estudios.TabIndex = 2;
             this.iconbtn_estudios.Text = "Estudios";
             this.iconbtn_estudios.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -106,10 +115,11 @@
             this.iconbtn_reportes.IconChar = FontAwesome.Sharp.IconChar.NotesMedical;
             this.iconbtn_reportes.IconColor = System.Drawing.Color.White;
             this.iconbtn_reportes.IconSize = 30;
-            this.iconbtn_reportes.Location = new System.Drawing.Point(0, 177);
+            this.iconbtn_reportes.Location = new System.Drawing.Point(0, 218);
+            this.iconbtn_reportes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.iconbtn_reportes.Name = "iconbtn_reportes";
             this.iconbtn_reportes.Rotation = 0D;
-            this.iconbtn_reportes.Size = new System.Drawing.Size(255, 50);
+            this.iconbtn_reportes.Size = new System.Drawing.Size(340, 62);
             this.iconbtn_reportes.TabIndex = 1;
             this.iconbtn_reportes.Text = "Reportes";
             this.iconbtn_reportes.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -126,10 +136,11 @@
             this.iconbtn_inicio.IconChar = FontAwesome.Sharp.IconChar.ClinicMedical;
             this.iconbtn_inicio.IconColor = System.Drawing.Color.White;
             this.iconbtn_inicio.IconSize = 30;
-            this.iconbtn_inicio.Location = new System.Drawing.Point(0, 127);
+            this.iconbtn_inicio.Location = new System.Drawing.Point(0, 156);
+            this.iconbtn_inicio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.iconbtn_inicio.Name = "iconbtn_inicio";
             this.iconbtn_inicio.Rotation = 0D;
-            this.iconbtn_inicio.Size = new System.Drawing.Size(255, 50);
+            this.iconbtn_inicio.Size = new System.Drawing.Size(340, 62);
             this.iconbtn_inicio.TabIndex = 0;
             this.iconbtn_inicio.Text = "Inicio";
             this.iconbtn_inicio.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -142,16 +153,18 @@
             this.panel_logo.Controls.Add(this.pictureBox1);
             this.panel_logo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel_logo.Location = new System.Drawing.Point(0, 0);
+            this.panel_logo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel_logo.Name = "panel_logo";
-            this.panel_logo.Size = new System.Drawing.Size(255, 127);
+            this.panel_logo.Size = new System.Drawing.Size(340, 156);
             this.panel_logo.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Capa_Presentacion.Properties.Resources.logo_imss;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(255, 118);
+            this.pictureBox1.Size = new System.Drawing.Size(340, 145);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -160,26 +173,68 @@
             // 
             this.panel_central.BackColor = System.Drawing.SystemColors.Control;
             this.panel_central.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel_central.Location = new System.Drawing.Point(255, 0);
+            this.panel_central.Location = new System.Drawing.Point(340, 0);
+            this.panel_central.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel_central.Name = "panel_central";
-            this.panel_central.Size = new System.Drawing.Size(1229, 761);
+            this.panel_central.Size = new System.Drawing.Size(1584, 937);
             this.panel_central.TabIndex = 1;
+            // 
+            // submenu_paciente
+            // 
+            this.submenu_paciente.Controls.Add(this.nuevo_paciente);
+            this.submenu_paciente.Controls.Add(this.lista_paciente);
+            this.submenu_paciente.Dock = System.Windows.Forms.DockStyle.Top;
+            this.submenu_paciente.Location = new System.Drawing.Point(0, 404);
+            this.submenu_paciente.Name = "submenu_paciente";
+            this.submenu_paciente.Size = new System.Drawing.Size(340, 100);
+            this.submenu_paciente.TabIndex = 0;
+            // 
+            // lista_paciente
+            // 
+            this.lista_paciente.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lista_paciente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lista_paciente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lista_paciente.Location = new System.Drawing.Point(0, 0);
+            this.lista_paciente.Name = "lista_paciente";
+            this.lista_paciente.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.lista_paciente.Size = new System.Drawing.Size(340, 45);
+            this.lista_paciente.TabIndex = 0;
+            this.lista_paciente.Text = "Lista de Pacientes";
+            this.lista_paciente.UseVisualStyleBackColor = true;
+            this.lista_paciente.Click += new System.EventHandler(this.lista_paciente_Click);
+            // 
+            // nuevo_paciente
+            // 
+            this.nuevo_paciente.Dock = System.Windows.Forms.DockStyle.Top;
+            this.nuevo_paciente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nuevo_paciente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.nuevo_paciente.Location = new System.Drawing.Point(0, 45);
+            this.nuevo_paciente.Name = "nuevo_paciente";
+            this.nuevo_paciente.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.nuevo_paciente.Size = new System.Drawing.Size(340, 45);
+            this.nuevo_paciente.TabIndex = 1;
+            this.nuevo_paciente.Text = "Nuevo Paciente +";
+            this.nuevo_paciente.UseVisualStyleBackColor = true;
+            this.nuevo_paciente.Click += new System.EventHandler(this.nuevo_paciente_Click);
             // 
             // Inicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1484, 761);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1924, 937);
             this.Controls.Add(this.panel_central);
             this.Controls.Add(this.panel_MenuLateral);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(1500, 800);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(1918, 974);
             this.Name = "Inicio";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.panel_MenuLateral.ResumeLayout(false);
             this.panel_logo.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.submenu_paciente.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -194,5 +249,8 @@
         private FontAwesome.Sharp.IconButton iconbtn_paciente;
         private FontAwesome.Sharp.IconButton iconbtn_estudios;
         private FontAwesome.Sharp.IconButton iconbtn_reportes;
+        private System.Windows.Forms.Panel submenu_paciente;
+        private System.Windows.Forms.Button nuevo_paciente;
+        private System.Windows.Forms.Button lista_paciente;
     }
 }
